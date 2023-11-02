28ecd1b2fef7dc688781126bd32d703ca5acfd07
aleja <aleja@Jeff>
2023-11-02 15:08:08 -0600

    avance Final final

---
+++ src/prueba1q4/BlockBuster.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prueba1q4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author aleja
+ */
+public class BlockBuster {
+    public static List<Block> BlockBuster;
+    public static String buscarItem;
+    public static String agregarItem;
+        
+   
+    
+    
+
+    public BlockBuster() {
+        BlockBuster = new ArrayList<>();
+        
+        
+    }
+    
+    public String buscarItem(int codigo, String tipo) {
+        
+        for (Object object : BlockBuster) {
+            if (BlockBuster =  "MOVIE" || BlockBuster =  "MOVIE") {
+                
+            } else {
+            }
+            
+        }
+       
+       
+         return null;
+    
+    }
+    
+    public String agregarItem(int codigo, String nombre, String tipoItem){
+    
+        
+        return null;
+    }
+    
+    public String Rentar(int codigo, String tipoItem, int dias){
+        return null; 
+    
+    }
+    
+    auditarMovieEstados().
+    
+}
